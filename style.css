7caef380b6b49a14ba843bd79e9df3c2422347c9
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2023-05-24 19:55:30 +0530

    Create style.css

---
+++ style.css
@@ -0,0 +1,141 @@
+/* Google fonts import link */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins',sans-serif;
+}
+body{
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #cdb47a;
+}
+.wrapper{
+  user-select: none;
+ 
+}
+.wrapper .heading {
+    display: flex;
+    font-size: 50px;
+    align-items: center;
+    justify-content: center;
+    color: #4d14ea;
+    position: relative; /* add position property */
+  }
+  
+  .wrapper .heading::after {
+    content: '';
+    position: absolute;
+    bottom: -1px;
+    left: 50%;
+    transform: translateX(-50%); /* center the line */
+    width: 200px;
+    height: 2px;
+    background-color: #5a0891;
+    border-radius: 2px;
+    animation: line-grow 2s ease-in-out infinite alternate;
+  }
+  
+  @keyframes line-grow {
+    0% {
+      transform: translateX(-50%) scale(1);
+    }
+    100% {
+      transform: translateX(-50%) scale(1.5); /* increase scale for animation */
+    }
+  }
+  
+  
+.wrapper .time {
+    height: 300px;
+    width: 300px;
+    background: #e8faf9;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 30px;
+    border-radius: 50%;
+    box-shadow: 10px 10px 20px rgba(164, 2, 2, 0.09);
+    padding: 0 10px;
+    animation: glowing 2s ease-in-out infinite;
+  }
+  
+  @keyframes glowing {
+    0% {
+      box-shadow: 0 0 10px #e8faf9;
+    }
+    50% {
+      box-shadow: 0 0 30px #772d06;
+    }
+    100% {
+      box-shadow: 0 0 10px #e8faf9;
+    }
+  }
+  
+.wrapper .time span{
+  width: 100px;
+  text-align: center;
+  font-size: 50px;
+  font-weight: 500;
+  color: black;
+  box-shadow: 1px 2px 21px 1px rgba(164, 2, 2, 0.09);
+}
+.time span.colon{
+  width: 10px;
+}
+.time span.ms-colon,
+.time span.millisecond{
+  color: #EAC7C7;
+}
+.wrapper .buttons{
+    text-align: center;
+    margin-top: 40px;
+    
+  }
+  .buttons button {
+    padding: 6px 16px;
+    outline: none;
+    border: none;
+    margin: 0 5px;
+    color: #EAC7C7;
+    background: #fff;
+    font-size: 19px;
+    font-weight: 500;
+    border-radius: 4px;
+    cursor: pointer;
+    box-shadow: 10px 10px 20px rgba(0,0,0,0.09);
+    transition: all 0.3s ease-in-out; 
+    /* add transition property */
+  }
+  
+  .buttons button:hover {
+    background-color: #5613d2;
+    color: #fff;
+    box-shadow: 5px 5px 10px rgba(0,0,0,0.3); 
+    /* add hover effect */
+    animation: glowing 1s ease-in-out infinite;
+  }
+  
+  .buttons button.active,
+  .buttons button.stopActive{
+    pointer-events: none;
+    opacity: 0.6;
+  }
+  footer {
+    background-color: #f8f8f8;
+    padding: 40px;
+    user-select: none;
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
+  
+  footer p {
+    margin: 0;
+    font-size: 20px;
+    color: #772d06;
+  }
